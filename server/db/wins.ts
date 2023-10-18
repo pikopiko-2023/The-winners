684a98a5ff270c8b42b6b9b2a08c74420923c973
Jayde Medder <jayde.medder@hotmail.com>
2023-10-18 15:12:17 +1300

    added db functions, added windata to models for new wins

--- server/db/wins.ts
+++ server/db/wins.ts
@@ -1,6 +1,20 @@
 import connection from './connection.ts'
-import { Win } from '../../models/wins.ts'
+import { Win, WinData } from '../../models/wins.ts'
 
-export async function getAllWins(db = connection): Promise<Win[]> {
+const db = connection
+
+export async function getAllWins(): Promise<Win[]> {
   return db('wins').select()
+}
+
+export async function getWinById(id: number): Promise<Win> {
+  return db('wins').where({ id }).first()
+}
+
+export async function getWinsByType(type: string): Promise<Win[]> {
+  return db('wins').where({ type }).select('*')
+}
+
+export async function addWin(newWin: WinData) {
+  return db('wins').insert(newWin)
 }
