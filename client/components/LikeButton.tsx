e7980f8b06fbdc3c51cd45452c3d7d222abb6e54
Richard <richard.hape@devacademy.co.nz>
2023-10-20 11:34:36 +1300

    Added LikeButton component

---
+++ client/components/LikeButton.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { FaHeart } from 'react-icons/fa'
+
+interface LikeButtonProps {
+  postId: string
+  // Need to sort out correct Id for posts & comments
+}
+
+// What is already created instead of postId?j
+const LikeButton: React.FC<LikeButtonProps> = ({ postId }) => {
+  const [liked, setLiked] = useState(false)
+
+  const handleLikeClick = () => {
+    setLiked(!liked)
+  }
+
+  return (
+    <div>
+      <button
+        onClick={handleLikeClick}
+        style={{ border: 'none ', backgroundColor: 'white' }}
+      >
+        <FaHeart color={liked ? 'pink' : 'gray'} size={20} />
+      </button>
+    </div>
+  )
+}
+
+export default LikeButton
