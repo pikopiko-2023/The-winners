80b77d1134af95c7314c26fb017aa3df58228e76
Hope Goldsack <hsgoldsack@gmail.com>
2023-10-20 11:36:08 +1300

    win typescript fix

--- client/components/WinDetails.tsx
+++ client/components/WinDetails.tsx
@@ -1,16 +1,15 @@
-
+import { Win } from '../../models/wins.tsx'
 // display a single win, pass win from winlist into this //
 interface Props {
   win: Win
 }
-
 
 export function WinDetails(props: Props) {
   const { win } = props
 
   return (
     <>
-      <div className= 'win-box'>
+      <div className="win-box">
         <h2>{win.title}</h2>
         <h3>{win.name}</h3>
         <h4>{win.date}</h4>
