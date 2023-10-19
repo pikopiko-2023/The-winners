0f4f653a78da12c4572780ead3454718a32da377
Richard <richard.hape@devacademy.co.nz>
2023-10-20 09:48:37 +1300

    Revert "LikeButton component"
    This reverts commit d4ebf76eaf150ae5ac1fdc886d3ff6a497b99d8a.

--- client/components/WinsList.tsx
+++ client/components/WinsList.tsx
@@ -1,14 +1,15 @@
 import { useWins } from '../hooks/useWins.ts'
 import { Win } from '../../models/wins.ts'
-import LikeButton from './LikeButton.tsx'
+
 
 function WinsList() {
-  const { data } = useWins()
-
+   const { data } = useWins()
+  
   return (
     <>
       <div className="section">
-        <div>
+        
+          <div>
           {data ? (
             data.map((win: Win, index: number) => (
               <div key={index}>
@@ -17,7 +18,6 @@ function WinsList() {
                 <h3>{win.name}</h3>
                 <h4>{win.date}</h4>
                 <p>{win.win}</p>
-                <LikeButton postId={win.id} />
                 <hr></hr>
               </div>
             ))
