12a77e8db05c85a60c9987c09cf7ca3f51201097
Laura Richards <lionheartnz85@gmail.com>
2023-10-21 15:50:02 +1300

    set up test-utils file

---
+++ client/test-utils.tsx
@@ -0,0 +1,23 @@
+import {beforeEach, expect} from 'vitest'
+import {cleanup, render } from '@testing-library/react'
+import * as matchers from '@testing-library/jest-dom/matchers'
+import {RouterProvider, createMemoryRouter } from 'react-router-dom'
+import {routes } from './routes.tsx'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+beforeEach(cleanup)
+expect.extend(matchers)
+
+
+
+export function renderRoute(location: string) {
+  const router = createMemoryRouter(routes, {initialEntries: [location],})
+}
+
+const queryClient = new QueryClient()
+
+render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+)
